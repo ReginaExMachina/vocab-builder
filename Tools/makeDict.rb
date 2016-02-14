55e814f15ed0390566859ad6d597955f196721c3
Rachel <we.love.rachel.day.lots@gmail.com>
2016-02-14 17:20:32 -0500

    Create makeDict.rb

---
+++ Tools/makeDict.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+wordList = Hash.new
+
+file = 'alphaList.rb'
+n = 0
+
+d = File.open(file, "r")
+d.each_line { |line|
+wordList.store(n, line) 
+n += 1 }
+
+d.close
+
+print wordList
+
+
+## OUTPUTS ---- New dict.txt
+#finalDict = "dict.txt"
+#endFile = File.open(finalDict, "w")
+#endFile.puts wordList
+#endFile.close
