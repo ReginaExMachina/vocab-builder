cdd5873d2fcd74fbde7be69c68066bd750c70c4f
Rachel <we.love.rachel.day.lots@gmail.com>
2016-06-21 23:26:47 -0400

    Update toyboxDict.py

--- toyboxDict.py
+++ toyboxDict.py
@@ -1,13 +1,18 @@
 # Public: Toybox verison of w3 program
 #
-# w3Dict - dictionary of interesting  words
-# w3DictDef - dictionary of definitions
-# *Keys for both dicts are matching integers for word/defintion.
+# w3_dict - dictionary of interesting words.
+# w3_defs - dictionary of definitions.
+#
+# Keys for both dicts are matching integers for word/defintion.
+#
+# word_display() - Displays a randomly generated word and its definition.
+# learning_listed - Removes displayed word from main list.
 
 # Returns a random set of 1: A word from the list and it's definition.
 
 
-w3Dict = { 1: 'amorous',
+w3_dict = { 
+          1: 'amorous',
           2: 'amorphous',
           3: 'antithesis',
           4: 'apostate',
@@ -23,43 +28,48 @@ w3Dict = { 1: 'amorous',
           14: 'cognomen',
           15: 'concise',
           16: 'corollary',
-          17: 'debonair'}
+          17: 'debonair'
+          }
 
-w3DictDef = { 1: '1: inclined toward or displaying love 2: expressive of or exciting sexual love or romance',
-             2: 'formless, shapeless, having no definite form or distinct shape',
-             3: '1: exact opposite 2: the juxtaposition of contrasting words or ideas to give a feeling of balance',
-             4: '1: a disloyal person who betrays or deserts his cause or religion or political party or friend etc. 2: not faithful to religion or party or cause',
-             5: '1: model of excellence or perfection of a kind; one having no equal 2: the elevation of a person ie. as to the status of a god',
-             6: 'characteristic of an enemy or one eager to fight',
-             7: 'doing or producing good',
-             8: '1: any of the salts of hydrobromic acid; formerly used as a sedative but now generally replaced by safer drugs 2: a trite or obvious remark',
-             9: 'pertaining to or having finely developed buttocks',
-             10: 'harshly critical or expressing censure',
-             11: 'an artificial reservoir for storing liquids; especially an underground tank for storing rainwater',
-             12: 'a supplement to a will; a testamentary instrument intended to alter an already executed will',
-             13: 'having or showing knowledge or understanding or realization or perception',
-             14: 'a familiar name for a person',
-             15: 'expressing much in few words',
-             16: '1: a practical consequence that follows naturally 2: an inference that follows directly from the proof of another proposition',
-             17: '1: having a sophisticated charm 2: having a cheerful, lively, and self-confident air' }
+w3_defs = {
+          1: '1: inclined toward or displaying love 2: expressive of or exciting sexual love or romance',
+          2: 'formless, shapeless, having no definite form or distinct shape',
+          3: '1: exact opposite 2: the juxtaposition of contrasting words or ideas to give a feeling of balance',
+          4: '1: a disloyal person who betrays or deserts his cause or religion or political party or friend etc. 2: not faithful to religion or party or cause',
+          5: '1: model of excellence or perfection of a kind; one having no equal 2: the elevation of a person ie. as to the status of a god',
+          6: 'characteristic of an enemy or one eager to fight',
+          7: 'doing or producing good',
+          8: '1: any of the salts of hydrobromic acid; formerly used as a sedative but now generally replaced by safer drugs 2: a trite or obvious remark',
+          9: 'pertaining to or having finely developed buttocks',
+          10: 'harshly critical or expressing censure',
+          11: 'an artificial reservoir for storing liquids; especially an underground tank for storing rainwater',
+          12: 'a supplement to a will; a testamentary instrument intended to alter an already executed will',
+          13: 'having or showing knowledge or understanding or realization or perception',
+          14: 'a familiar name for a person',
+          15: 'expressing much in few words',
+          16: '1: a practical consequence that follows naturally 2: an inference that follows directly from the proof of another proposition',
+          17: '1: having a sophisticated charm 2: having a cheerful, lively, and self-confident air' 
+          }
 
 
 from random import randint
 
 # INTIALIZING VARIABLES
-
-n = len(w3Dict)
+n = len(w3_dict) #May switch for randrange
 choice = ''
 
-def wordDisplay():
+# MAKING METHODS
+def word_display():
     word = randint(1,n)
-    print w3Dict[word]
-    print w3DictDef[word]
-    nowLearning(word)
+    #word = random.randrange(0, len(w3_dict))
 
-def nowLearning(word):
-    learningList = w3Dict.pop(word)
-    learningDefsList = w3DictDef.pop(word)
+    print w3_dict[word]
+    print w3_def[word]
+    learning_listed(word)
+
+def learning_listed(word):
+    learning_list = w3_dict.pop(word)
+    learning_defs_list = w3_def.pop(word)
 
 
 print("W3")
@@ -67,6 +77,6 @@ print('Press E to Exit or any key to reload.\n')
 
 while choice != 'E':
     
-    wordDisplay()
+    word_display()
     choice = raw_input('\n ')
     print(' ')
